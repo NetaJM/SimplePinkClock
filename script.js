5357cc876b634c73bb9e41c4998cadf626ef01e1
Neta Milenkovic <92453633+NetaJM@users.noreply.github.com>
2022-02-20 13:45:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+const hour = document.querySelector('.hour');
+const minute = document.querySelector('.minute');
+const second = document.querySelector('.second');
+
+function setDate() {
+
+  const currTime = new Date ();
+
+const getSecond = currTime.getSeconds();
+const getMinute = currTime.getMinutes();
+const getHour = currTime.getHours();
+
+// converting the times into degrees for the portion of the movements of each arrow 
+const secondsInDegrees = (getSecond / 60) * 360;
+const minutesInDegrees = (getMinute / 60) * 360;
+const hoursInDegrees = (getHour / 60) * 360;
+
+second.style.transform = `rotate(${secondsInDegrees}deg)`;
+minute.style.transform = `rotate(${minutesInDegrees}deg)`;
+hour.style.transform = `rotate(${hoursInDegrees}deg)`;
+
+
+};
+
+setInterval(setDate, 1000);
+
